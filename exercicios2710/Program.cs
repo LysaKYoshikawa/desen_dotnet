bed207dc278faf0f7f89144acd3fe1559a670021
Monalysa Klauck Yoshikawa <monalysa.yoshikawa@gruposbf.com.br>
2022-12-07 01:14:32 -0300

    Subindo aula 27

---
+++ exercicios2710/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BoxingUboxingTeste
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int i = 777; //tipo primitivo / tipo de valor
+                object o = i; //oxing i->o (estou jogando o i para dentro "o" ou seja int para objecto)
+                int j = (int)o; //unboxing de "o" para "j" 
+                Console.WriteLine("Realizado com sucesso");
+
+                Console.WriteLine("Data atual" + DateTime.Now);
+                Console.WriteLine("Data atual minimo " + DateTime.MinValue); //tera uma data minima e valor minimo
+                Console.WriteLine("Data atual minimo " + DateTime.MaxValue); //tera o valor maximo
+
+                DateTime data;
+
+                DateTime finalAno = new DateTime(2022, 12, 31);
+
+                Console.WriteLine("Faltam :" + finalAno.Subtract(DateTime.Now) + "dias para acabar o ano!!!");
+
+                throw new Exception("Lançado uma exception"); //erro critico, lançado exception
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Aconteceu um erro na aplicação: " + ex.Message);
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ exercicios_aula/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicios_aula
+{
+    internal class Class1
+    {
+    }
+}
