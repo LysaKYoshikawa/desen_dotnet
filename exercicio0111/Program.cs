7414fb2f36de0bc6edf7319c5a534d3719f7c5d2
Monalysa Klauck Yoshikawa <monalysa.yoshikawa@gruposbf.com.br>
2022-12-11 02:28:13 -0300

    feat/subindo-aulas-c#

---
+++ exercicio0111/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace interacoes
+{
+    class exercicio
+    {
+        static void Main(String[] args)
+        {
+            try
+            {
+                int numero = 5656;
+                string nome = "Monalysavvv";
+
+                if(numero == 565)
+                {
+                    Console.WriteLine("É igual ao valor " + numero);
+                }
+                else
+                {
+                    Console.WriteLine("Não É igual ao valor " + numero);
+                }
+                if(numero==5656 && nome =="Monalysa")
+                {
+                    Console.WriteLine("O numero é: " + numero + " e o nome é igual a " + nome);
+                }
+                else
+                {
+                    Console.WriteLine("Não passou na validação");
+                }
+                Console.ReadKey();
+
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+    }
+}
